cd9e4a5fae0e327e36329913f5264a08ffd13c0e
cbwuyanzu <277850402@qq.com>
2016-07-06 12:54:42 -0400

    strStr new

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StrStr s = new StrStr();
+		int i = s.strStr("tartarget", "target");
+		System.out.print(i);
+	}
+
+}
--- src/StrStr.java
+++ src/StrStr.java
@@ -1,17 +1,35 @@
-class strStr {
-    /**
-     * Returns a index to the first occurrence of target in source,
-     * or -1  if target is not part of source.
-     * @param source string to be scanned.
-     * @param target string containing the sequence of characters to match.
-     */
+class StrStr {
+	/**
+	 * Returns a index to the first occurrence of target in source, or -1 if
+	 * target is not part of source.
+	 * 
+	 * @param source
+	 *            string to be scanned.
+	 * @param target
+	 *            string containing the sequence of characters to match.
+	 */
 	public int strStr(String source, String target) {
 		// write your code here
 		if (source == null || target == null) {
 			return -1;
 		}
-		if (source.indexOf(target) >= 0) {
-			return source.indexOf(target);
+		if (target.length() == 0) {
+			return 0;
+		}
+		if (target.length() > source.length()) {
+			return -1;
+		}
+		for (int i = 0; i < source.length() - target.length() + 1; i++) {
+			boolean flag = true;
+			for (int j = 0; j < target.length(); j++) {
+				if (target.charAt(j) - source.charAt(j + i) != 0) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) {
+				return i;
+			}
 		}
 		return -1;
 	}
